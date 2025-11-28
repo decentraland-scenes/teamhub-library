144d3267002a4ebc7a87dad7b3b3784f51f1ccf3
Giorgio <65674128+Gaffomdz@users.noreply.github.com>
2025-11-28 15:09:44 +0100

    feat: ignore-entities-default-config (#6)
    * feat: ignore-entities-default-config
    
    * remove-comments
    
    * ignore-main-composite

--- src/start.ts
+++ src/start.ts
@@ -2,9 +2,12 @@ import { GameController } from './controllers/game.controller'
 import './uis/ui.registry'
 export * from './controllers/ui.controller'
 
+export type TeamHubOptions = {
+  ignoreModels?: boolean
+}
 export class TeamHub {
   gameController = new GameController()
-  constructor() {
-    this.gameController.start()
+  constructor(options: TeamHubOptions = {}) {
+    this.gameController.start(options)
   }
 }
